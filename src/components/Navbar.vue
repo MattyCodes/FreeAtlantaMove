7a3c885e70959917ab9f43dd9f17f1b26b9ed091
mattyfagioli <mattyfagioli@penguin>
2019-09-25 07:50:30 -0400

    Updates favicon, adds necessary assets (images, styles and scripts), wraps up navbar styles

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,26 @@
 <template>
-  <div v-if="navbarShouldBeShown">
-    <router-link to="/"> Home </router-link> |
-    <router-link to="/about"> About </router-link> |
-    <router-link to="/contact"> Contact </router-link>
-  </div>
+  <nav id="application-navbar" class="navbar navbar-default" role="navigation" v-if="navbarShouldBeShown">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button class="navbar-toggle" data-target="#application-navbar-collapse" data-toggle="collapse" type="button">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <router-link class="navbar-brand" to="/">
+          <img src="../assets/fam-nav-logo.png" />
+        </router-link>
+      </div>
+      <div id="application-navbar-collapse" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav"> <!-- Left-aligned links can be rendered from this list. --> </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><router-link to="/"> Home </router-link></li>
+          <li><router-link to="/about"> About </router-link></li>
+          <li><router-link to="/contact"> Contact </router-link></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,95 @@
   </div>
 </template>
 
+<!-- .container-fluid.full-height.homepage-wrapper.text-center.relative
+  .homepage-copyright-footer © 2019 FreeAtlantaMove.com
+  .row
+    .col-sm-12.col-md-12.col-lg-12
+      = image_tag 'fam-truck-with-white-text.png', class: 'homepage-logo'
+      %p.homepage-form-header You are only one step from a FREE move!
+    .col-sm-12.col-md-12.col-lg-12
+      = form_tag '/', method: :get do
+        = label_tag 'Email:', nil, class: 'homepage-form-label'
+        = text_field_tag 'email', nil, class: 'homepage-form-input'
+        = submit_tag 'Submit', class: 'homepage-form-submit' -->
+
+
+<style>
+  .homepage-wrapper {
+    /* TODO: Resolve path to homepage background image. */
+    /* background: url('fam-homepage-background.jpg'); */
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+
+  .homepage-logo {
+    width: 50%;
+    min-width: 290px;
+    max-width: 600px;
+    margin: 90px 0 55px 0;
+  }
+
+  .homepage-form-header {
+    color: #F5F5F5;
+    font-size: 20px;
+    margin: 0 0 20px 0;
+  }
+
+  .homepage-form-label {
+    color: #F5F5F5;
+    display: inline-block;
+    font-size: 17px;
+    padding-right: 10px;
+  }
+
+  .homepage-form-input {
+    outline: none !important;
+    border: 1px solid #e3e3e3;
+    padding: 5px 11px;
+    width: 100%;
+    max-width: 230px;
+    border-radius: 4px;
+    transition: all 250ms;
+  }
+
+  .homepage-form-input:focus,
+  .homepage-form-input:active,
+  .homepage-form-input:hover {
+    border: 1px solid #0275db;
+  }
+
+  .homepage-form-submit {
+    display: block;
+    margin: 20px auto;
+    border: 2px solid #F5F5F5;
+    background: #ca2626;
+    color: #F5F5F5;
+    font-weight: 600;
+    font-size: 15px;
+    border-radius: 50px;
+    padding: 6px 33px;
+    outline: none !important;
+    text-transform: uppercase;
+    letter-spacing: 0.5px;
+    transition: all 250ms;
+  }
+
+  .homepage-form-submit:hover,
+  .homepage-form-submit:active,
+  .homepage-form-submit:focus {
+    filter: brightness(95%);
+  }
+
+  .homepage-copyright-footer {
+    position: absolute;
+    text-align: left;
+    bottom: 30px;
+    left: 45px;
+    color: #F5F5F5;
+  }
+</style>
+
 <script>
   import axios from "axios";
 
