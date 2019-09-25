b07600ad05a933ef82c9075bf5e7e7d42255c034
MattyCodes <matt.nick.fagioli@gmail.com>
2019-09-25 08:05:52 -0500

    Adds express-history functionality to basic server to that pages can be loaded directly in production

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const express     = require('express');
-const serveStatic = require("serve-static")
-const path        = require('path');
-app               = express();
+const express              = require('express');
+const serveStatic          = require("serve-static")
+const history              = require('connect-history-api-fallback');
+const path                 = require('path');
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+app                        = express();
 
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 8080;
-app.listen(port);
+app.use(staticFileMiddleware);
+app.use(history({ disableDotRule: true, verbose: true }));
+app.use(staticFileMiddleware);
+
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/index.html'));
+});
+
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+});
